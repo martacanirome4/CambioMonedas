63ae12b8f5846495b03d27ad12205536b0ae046a
MartaCanino <martacaninoromero@gmail.com>
2023-06-08 22:04:15 +0200

    Iterat y rec correcto lambda error

--- src/main/java/app/org/AppCambio.java
+++ src/main/java/app/org/AppCambio.java
@@ -2,6 +2,7 @@ package org.pr2;
 
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 
 public class AppCambio {
@@ -18,7 +19,28 @@ public class AppCambio {
         // el trabajo al alumno, no será necesario comprobar que `cambio >= 0` ni
         // que el sistema de monedas es válido.
         int resultado = 0;
-        // CÓDIGO A COMPLETAR
+        // CÓDIGO A 
+        /**
+         * for (int moneda : sistemaMonedas) {
+                if (moneda <= cambio) {
+                    resultado += 1;
+                    cambio -= moneda;
+                }
+            }
+         * **/
+        // CÓDIGO B
+        for(int i = 0; i < sistemaMonedas.size(); i++){
+            while (cambio > 0){
+                cambio -= sistemaMonedas.get(i);
+                resultado++;
+            }
+            if (cambio != 0){
+                cambio += sistemaMonedas.get(i);
+                resultado --;
+            } 
+        }
+
+
         return resultado; 
     }
 
@@ -32,6 +54,9 @@ public class AppCambio {
     public static int cambioRecursivo(int cambio, List<Integer> sistemaMonedas) {
         int[] resultado = new int[1]; // Esta variable se puede utilizar para
         // paso por referencia. cambioRecursivo(cambio, 0, sistemaMonedas, resultado);
+
+        cambioRecursivo(cambio, 0, sistemaMonedas, resultado);
+
         return resultado[0];
     }
 
@@ -39,6 +64,41 @@ public class AppCambio {
         // `i` es el índice que se utiliza para recorrer el sistema de monedas.
         // En `resultado[0]` se van acumulando los resultados de las llamadas recursivas.
         // CÓDIGO A COMPLETAR
+
+        if (cambio == 0) {
+            return;
+        }
+
+        if (i < sistemaMonedas.size() && sistemaMonedas.get(i) <= cambio) {
+            // Si el valor de la moneda en el índice actual es menor o igual al cambio,
+            // se puede utilizar esa moneda para el cambio.
+            resultado[0]++;
+            cambio -= sistemaMonedas.get(i);
+        } else {
+            // Si no se puede utilizar la moneda actual, se pasa al siguiente índice.
+            i++;
+        }
+
+        // Llamada recursiva con el nuevo cambio y el siguiente índice.
+        cambioRecursivo(cambio, i, sistemaMonedas, resultado);
+
+
+    }
+
+    /**
+    * Dado un cambio de dinero y un sistema de monedas ordenado de mayor a menor * (todo en céntimos), devolver el número de monedas mínimo correspondiente al
+    * cambio. Esta función debe ser implementada con expresiones lambda.
+    * @param cambio
+    * @param sistemaMonedas
+    * @return el número mínimo de monedas.
+    */
+    public static int cambioLambda(int cambio, List<Integer> sistemaMonedas) {
+        return sistemaMonedas.stream()
+                            .reduce(0, (count, moneda) -> {
+                                int numCoins = cambio / moneda;
+                                cambio %= moneda;
+                                return count + numCoins;
+                            }, Integer::sum);
     }
 
 }
--- src/test/java/app/org/AppTest.java
+++ src/test/java/app/org/AppTest.java
@@ -17,10 +17,49 @@ public class AppTest {
 
     @Test
     public void cambioIterativo250Ok() {
-        
         // TODO Completar assertEquals(1,1);
+        int cambio = 250;
+        int expected = 2; // Expected minimum number of coins needed for 250 cents
+
+        int actual = AppCambio.cambioIterativo(cambio, sistemaMonedas);
+        assertEquals(expected, actual);
     }
 
-    // ESCRIBIR MÁS TESTS
+    @Test
+    public void cambioRecursivo250Ok() {
+        int cambio = 250;
+        int expected = 2; // Expected minimum number of coins needed for 250 cents
+
+        int actual = AppCambio.cambioRecursivo(cambio, sistemaMonedas);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cambioLambda250Ok() {
+        int cambio = 250;
+        int expected = 2; // Expected minimum number of coins needed for 250 cents
+
+        int actual = AppCambio.cambioLambda(cambio, sistemaMonedas);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cambioRecursivo50Ok() {
+        int cambio = 50;
+        int expected = 1; // Expected minimum number of coins needed for 50 cents
+
+        int actual = AppCambio.cambioRecursivo(cambio, sistemaMonedas);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cambioRecursivo0Ok() {
+        int cambio = 0;
+        int expected = 0; // No change needed
+
+        int actual = AppCambio.cambioRecursivo(cambio, sistemaMonedas);
+        assertEquals(expected, actual);
+    }
+
 
 }
